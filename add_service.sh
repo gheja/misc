1319ec3b2ecff776a100bed94f0a496351cf4bfc
Gabor Heja <gheja@m00box.herd>
2019-02-15 13:16:00 +0100

    Added add_service.sh - to add a new systemd service

---
+++ add_service.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# != 3 ]; then
+	echo "$0 <name> <user> <command>"
+	exit 1
+fi
+
+name="$1"
+user="$2"
+command="$3"
+
+file="/etc/systemd/system/$name.service"
+
+if [ -e "$file" ]; then
+	echo "$file: exists, exiting."
+	exit 1
+fi
+
+cat > $file <<EOF
+[Unit]
+Description=$name
+After=network.target
+
+[Service]
+Type=simple
+User=$user
+ExecStart=$command
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable $name
+systemctl start $name
